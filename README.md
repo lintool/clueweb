5d092f932d1735592de58c7f1d062c69b58c63be
Claudia Hauff <claudia.hauff@gmail.com>
2013-07-19 02:43:25 +0200

    Updated retrieval model; baseline run result added

--- README.md
+++ README.md
@@ -3,15 +3,17 @@ ClueWeb Tools (Fork) - Retrieval models
 
 This is a collection of tools for manipulating the [ClueWeb12 collection](http://lemurproject.org/clueweb12/).
 
-The code is forked (and updated) based on [Jimmy Lin's clueweb repository](https://github.com/lintool/clueweb).
+The code is forked from [Jimmy Lin's clueweb repository](https://github.com/lintool/clueweb). The only change occured in the addition of a retrieval app
+
++ org.clueweb.clueweb12.app.LMRetrieval
+
 
 Retrieval
 ---------
 
-Implemented is currently only the basic language modeling approach to IR; smoothing types are linear interpolation or Dirichlet.
+Implemented is currently the basic language modeling approach to IR; smoothing types are linear interpolation and Dirichlet.
 
 To run the code first follow the installation guideline of the original [clueweb repository]((https://github.com/lintool/clueweb) and build the dictionary and document vectors as described.
-
 
 To conduct a retrieval run, call:
 
@@ -20,13 +22,13 @@ $ hadoop jar clueweb-tools-X.X-SNAPSHOT-fatjar.jar \
 	org.clueweb.clueweb12.app.LMRetrieval \
 	-dictionary /data/private/clueweb12/derived/dictionary.20130710 \
 	-smoothing 1000 \
-	-output /user/claudiah/retrieved \
-	-queries /user/claudiah/web.queries.trec2013 \
+	-output /user/chauff/retrieved \
+	-queries /user/chauff/web.queries.trec2013 \
 	-vbdocvector /data/private/clueweb12/derived/docvectors.20130710/segm*/part* \
 	-topk 1000
 ``` 
 
-The parameters are as follows
+The parameters are:
 + `dictionary`: HDFS path to the dictionary created by the clueweb tools
 + `smoothing`: the smoothing parameter in the LM-based retrieval model; a value of <=1 automatically backs off to smoothing with linear interpolation while a value >1 runs Dirichlet smoothing (default is 1000)
 + `output`: folder in which the TREC results are collected (in TREC format); to merge everything into one file in the end call `hadoop fs -getmerge /path/to/output filename`; the resulting file should run smoothly through `trec_eval`
@@ -37,10 +39,119 @@ The parameters are as follows
 
 Retrieval runs
 --------------
-The folder `runs` contains the two baselines when running the above command with `smoothing 0.5` and `smoothing 1000` respectively.
+The folder `runs` contains the baseline run when running the above retrieval program.
+
+On an empty sara cluster, this run on 50 queries takes about one hour.
 
 
 Sanity check
 ------------
-To have confidence in the implementation, these baseline runs where compared with the [official organizer baselines](https://github.com/trec-web/trec-web-2013/tree/master/data/runs/baselines/2013/ql) (an Indri run) provided by the TREC's Web track organizers.
+To have confidence in the implementation, the baseline run was compared with the [official 2013 baselines](https://github.com/trec-web/trec-web-2013/tree/master/data/runs/baselines/2013/ql) (Indri runs) provided by the TREC's Web track organizers.
+
+Since no relevance juddgments are available for ClueWeb12, we report the overlap in document ids among the top 10 / top 1000 ranked documents for each query between our baseline and the organizer ql baseline (results-catA.txt). For about half of the queries, the overlap is 50% or more, indicating the code to be viable (differences can be explained through differences in HTML parsing, tokenization, stopwording, etc.)
+
+Percentage of overlap up to rank 10: 50
+Query 201 => overlap: 90%
+Query 202 => overlap: 60%
+Query 203 => overlap: 60%
+Query 204 => overlap: 20%
+Query 205 => overlap: 30%
+Query 206 => overlap: 70%
+Query 207 => overlap: 0%
+Query 208 => overlap: 60%
+Query 209 => overlap: 30%
+Query 210 => overlap: 50%
+Query 211 => overlap: 20%
+Query 212 => overlap: 30%
+Query 213 => overlap: 90%
+Query 214 => overlap: 60%
+Query 215 => overlap: 10%
+Query 216 => overlap: 20%
+Query 217 => overlap: 30%
+Query 218 => overlap: 50%
+Query 219 => overlap: 0%
+Query 220 => overlap: 10%
+Query 221 => overlap: 40%
+Query 222 => overlap: 90%
+Query 223 => overlap: 100%
+Query 224 => overlap: 40%
+Query 225 => overlap: 80%
+Query 226 => overlap: 70%
+Query 227 => overlap: 0%
+Query 228 => overlap: 10%
+Query 229 => overlap: 60%
+Query 230 => overlap: 0%
+Query 231 => overlap: 0%
+Query 232 => overlap: 80%
+Query 233 => overlap: 70%
+Query 234 => overlap: 70%
+Query 235 => overlap: 60%
+Query 236 => overlap: 70%
+Query 237 => overlap: 90%
+Query 238 => overlap: 70%
+Query 239 => overlap: 70%
+Query 240 => overlap: 80%
+Query 241 => overlap: 0%
+Query 242 => overlap: 60%
+Query 243 => overlap: 40%
+Query 244 => overlap: 70%
+Query 245 => overlap: 50%
+Query 246 => overlap: 30%
+Query 247 => overlap: 80%
+Query 248 => overlap: 50%
+Query 249 => overlap: 0%
+Query 250 => overlap: 90%
+
+
+ercentage of overlap up to rank 1000: 62%
+Query 201 => overlap: 84%
+Query 202 => overlap: 87.7%
+Query 203 => overlap: 66%
+Query 204 => overlap: 70.3%
+Query 205 => overlap: 45.7%
+Query 206 => overlap: 85.3%
+Query 207 => overlap: 14.8%
+Query 208 => overlap: 89%
+Query 209 => overlap: 57.4%
+Query 210 => overlap: 80.9%
+Query 211 => overlap: 22%
+Query 212 => overlap: 46%
+Query 213 => overlap: 91.6%
+Query 214 => overlap: 66.8%
+Query 215 => overlap: 53.1%
+Query 216 => overlap: 50.4%
+Query 217 => overlap: 62.6%
+Query 218 => overlap: 59%
+Query 219 => overlap: 0%
+Query 220 => overlap: 24%
+Query 221 => overlap: 68.6%
+Query 222 => overlap: 72.8%
+Query 223 => overlap: 80.6%
+Query 224 => overlap: 46.7%
+Query 225 => overlap: 88%
+Query 226 => overlap: 87.9%
+Query 227 => overlap: 2.8%
+Query 228 => overlap: 41.6%
+Query 229 => overlap: 79.8%
+Query 230 => overlap: 0%
+Query 231 => overlap: 28.1%
+Query 232 => overlap: 62.8%
+Query 233 => overlap: 86.2%
+Query 234 => overlap: 85.1%
+Query 235 => overlap: 75.9%
+Query 236 => overlap: 73.8%
+Query 237 => overlap: 51.5%
+Query 238 => overlap: 63.1%
+Query 239 => overlap: 92.4%
+Query 240 => overlap: 45.1%
+Query 241 => overlap: 1.6%
+Query 242 => overlap: 88.6%
+Query 243 => overlap: 81.2%
+Query 244 => overlap: 91.5%
+Query 245 => overlap: 78.2%
+Query 246 => overlap: 72%
+Query 247 => overlap: 56.3%
+Query 248 => overlap: 62.5%
+Query 249 => overlap: 1.5%
+Query 250 => overlap: 53%
 
--- src/main/java/org/clueweb/clueweb12/app/LMRetrieval.java
+++ src/main/java/org/clueweb/clueweb12/app/LMRetrieval.java
@@ -21,22 +21,26 @@
  * 
  * Approach:
  * 
- * (1) read the queries and convert into termids (based on the dictionary)
- * (2) MyMapper: walk over all document vectors and as soon as a termid is found in a document which occurs in a query,
- * 		output: composite key (qid,docid) and value score
- * (3) MyPartitioner: ensures that all keys (qid,docid) with the same qid end up in the same reducer
- * 		TODO: sanity check => are the (qid,docid) objects sorted as expected?
- * (4) MyReducer: keep track of the current query and a priority queue (min-heap); for each (qid,docid) key, sum up all scores
- * 		and if the heap is not at maximum capacity or the current score is larger than the heap's current minimum, add the score to the heap;
- * 		once a new qid is encountered, write out what is in the heap (scores and docids) for that query; reset everything and process the next query
+ * (1) read the queries and convert into termids (based on the dictionary);
+ * 			make sure to use the same Lucene Analyzer as in ComputeTermStatistics.java
  * 
- * TODO: queries with quasi-stopwords slow down the process a lot
+ * (2) MyMapper: walk over all document vectors
+ * 			2.1 determine all queries which have at least one query term is occurring in the document
+ * 			2.2 for each such query, compute the LM score and emit composite key: (qid,docid), value: (score)
+ * 
+ * (3) MyPartitioner: ensure that all keys (qid,docid) with the same qid end up in the same reducer
+ * 
+ * (4) MyReducer: for each query
+ * 			4.1 create a priority queue (minimum heap): we only need to keep the topk highest probability scores
+ * 			4.2 once all key/values for a query are processed, "sort" the doc/score elements in the priority queue (already semi-done in heap)
+ * 			4.3 output the results for a query in TREC result file format
+ * 
+ * TODO: queries with (quasi-)stopwords slow down the process a lot
  */
 
 package org.clueweb.clueweb12.app;
 
 import java.io.BufferedReader;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -61,10 +65,8 @@ import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.hadoop.io.FloatWritable;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.WritableComparator;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Partitioner;
@@ -75,12 +77,16 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.util.Version;
 import org.clueweb.data.TermStatistics;
 import org.clueweb.data.VByteDocVector;
 import org.clueweb.dictionary.DefaultFrequencySortedDictionary;
 
 import tl.lin.data.pair.PairOfIntString;
 import tl.lin.data.pair.PairOfStringFloat;
+import tl.lin.lucene.AnalyzerUtils;
 
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
@@ -91,7 +97,7 @@ public class LMRetrieval extends Configured implements Tool {
 	
 	
 	/*
-	 * everything with the same qid goes to the same reducer
+	 * Partitioner: all keys with the same qid go to the same reducer
 	 */
 	private static class MyPartitioner extends Partitioner<PairOfIntString, FloatWritable> {
 
@@ -100,10 +106,11 @@ public class LMRetrieval extends Configured implements Tool {
 				int numPartitions) {
 			return arg0.getLeftElement()%numPartitions;
 		}
-	 
 	}
 	
-	
+	/*
+	 * comparator for the priority queue: elements (docid,score) are sorted by score
+	 */
 	private static class CustomComparator implements Comparator<PairOfStringFloat>
 	{
 		@Override
@@ -119,42 +126,46 @@ public class LMRetrieval extends Configured implements Tool {
 	
 
 	/*
-	 * Mapper outKey: query id (integer) Mapper outValue: two part element:
-	 * documentid probability
+	 * Mapper outKey: (qid,docid), value: probability score
 	 */
 	private static class MyMapper extends
 			Mapper<Text, BytesWritable, PairOfIntString, FloatWritable> {
+		
 		private static final VByteDocVector DOC = new VByteDocVector();
-
 		private DefaultFrequencySortedDictionary dictionary;
 		private TermStatistics stats;
+		private double smoothingParam;
+		private static final Analyzer ANALYZER = new StandardAnalyzer(Version.LUCENE_43);
 
-		private double smoothing;
-
-		// key: termid, hashset value: qids where the termid occurs in
+		/* for quick access store the queries in two hashmaps:
+		 * 		1. key: termid, value: list of queries in which the termid occurs
+		 * 		2. key: qid, value: list of termids that occur in the query
+		*/
 		private HashMap<Integer, HashSet<Integer>> termidQuerySet;
 		private HashMap<Integer, HashSet<Integer>> queryTermidSet;
 
-		private static final PairOfIntString keyOut = new PairOfIntString();// qid
-																			// and
-																			// docid
+		//complex key: (qid,docid)
+		private static final PairOfIntString keyOut = new PairOfIntString();
+		//value: float; probability score log(P(q|d))
 		private static final FloatWritable valueOut = new FloatWritable();// score
 
+		
 		@Override
 		public void setup(Context context) throws IOException {
+			
 			FileSystem fs = FileSystem.get(context.getConfiguration());
 			String path = context.getConfiguration().get(DICTIONARY_OPTION);
 			dictionary = new DefaultFrequencySortedDictionary(path, fs);
 			stats = new TermStatistics(new Path(path), fs);
 
 			try {
-				smoothing = Double.parseDouble(context.getConfiguration().get(
+				smoothingParam = Double.parseDouble(context.getConfiguration().get(
 						SMOOTHING));
 			} catch (NumberFormatException e) {
 				LOG.info("Smoothing is expected to parse into a double, found: "
 						+ context.getConfiguration().get(SMOOTHING));
 				LOG.info("Smoothing set to 1000 (thus Dirichlet smoothing) due to an error in parsing!");
-				smoothing = 1000;
+				smoothingParam = 1000;
 			}
 
 			// read the queries from file
@@ -174,9 +185,8 @@ public class LMRetrieval extends Configured implements Tool {
 				int qid = Integer.parseInt(line.substring(0, index));
 				HashSet<Integer> termidSet = Sets.newHashSet();
 				
-				String terms[] = line.substring(index + 1).split("\\s");
-
-				for (String term : terms) {
+				//normalize the terms (same way as the documents)
+		        for (String term : AnalyzerUtils.parse(ANALYZER, line.substring(index + 1))) {
 					int termid = dictionary.getId(term);
 					if (termid < 0) {
 						LOG.info("Query " + qid + ": term [" + term
@@ -207,6 +217,7 @@ public class LMRetrieval extends Configured implements Tool {
 
 			VByteDocVector.fromBytesWritable(bytes, DOC);
 			
+			//determine which queries we care about for this document
 			HashSet<Integer> queriesToDo = Sets.newHashSet();
 
 			//tfMap of the document
@@ -218,18 +229,18 @@ public class LMRetrieval extends Configured implements Tool {
 				tfMap.put(termid, tf);
 				
 				if(termidQuerySet.containsKey(termid))
-				{
 					for(int qid : termidQuerySet.get(termid))
 						queriesToDo.add(qid);
-				}
 			}
 			
-			//we now know all queries for which term statistics need to be emitted
+			//for each of the interesting queries, compute log(P(q|d))
 			for(int qid : queriesToDo)
 			{
+				double score = 0.0;
+				
 				for(int termid : queryTermidSet.get(qid))
 				{
-					double tf = 0f;
+					double tf = 0.0;
 					if(tfMap.containsKey(termid))
 						tf = tfMap.get(termid);
 					double df = stats.getDf(termid);
@@ -240,38 +251,21 @@ public class LMRetrieval extends Configured implements Tool {
 					double prob = 0.0;
 
 					// JM smoothing
-					if (smoothing <= 1.0)
-						prob = smoothing * mlProb + (1.0 - smoothing) * colProb;
+					if (smoothingParam <= 1.0)
+						prob = smoothingParam * mlProb + (1.0 - smoothingParam) * colProb;
 					// Dirichlet smoothing
 					else
-						prob = (double) (tf + smoothing * colProb)
-								/ (double) (DOC.getLength() + smoothing);
+						prob = (double) (tf + smoothingParam * colProb)
+								/ (double) (DOC.getLength() + smoothingParam);
 
-					prob = Math.log(prob);
-
-					keyOut.set(qid, key.toString());
-					valueOut.set((float) prob);
-					context.write(keyOut, valueOut);
+					score += (float)Math.log(prob);
 				}
+
+				keyOut.set(qid, key.toString());
+				valueOut.set((float) score );
+				context.write(keyOut, valueOut);
 			}
 		}
-	}
-	
-	private static class MyCombiner extends
-	Reducer<PairOfIntString, FloatWritable, PairOfIntString, FloatWritable> {
-		
-		private static final FloatWritable valueOut = new FloatWritable();
-	
-		public void reduce(PairOfIntString key, Iterable<FloatWritable> values,
-				Context context) throws IOException, InterruptedException {
-			
-			float sum = 0f;
-			for(FloatWritable v : values)
-				sum += v.get();
-			valueOut.set(sum);
-			context.write(key, valueOut);
-		}
-
 	}
 
 
@@ -279,7 +273,8 @@ public class LMRetrieval extends Configured implements Tool {
 			Reducer<PairOfIntString, FloatWritable, NullWritable, Text> {
 
 		private int topk;
-		private PriorityQueue<PairOfStringFloat> queue;//docid score
+		//PairOfStringFloat is (docid,score)
+		private PriorityQueue<PairOfStringFloat> queue;
 		private int currentQuery;
 		private static final NullWritable nullKey = NullWritable.get();
 		private static final Text valueOut = new Text();
@@ -302,15 +297,12 @@ public class LMRetrieval extends Configured implements Tool {
 		public void reduce(PairOfIntString key, Iterable<FloatWritable> values,
 				Context context) throws IOException, InterruptedException {
 
-			// key: (qid,docid)
-			// value: score
-
+			//we hit a new query, process what is currently in the priority queue
 			if(key.getLeftElement()!=currentQuery)
 			{
-				//output what is currently in the priority queue
 				if(currentQuery>0)
 				{
-					//convert minimum heap into list with ascending order
+					//convert minimum heap into list in ascending order
 					List<PairOfStringFloat> orderedList = new ArrayList<PairOfStringFloat>();
 					while(queue.size()>0)
 						orderedList.add(queue.remove());
@@ -329,27 +321,25 @@ public class LMRetrieval extends Configured implements Tool {
 				currentQuery = key.getLeftElement();
 			}
 			
+			//actually, it should only be a single element
 			float scoreSum = 0f;
 			for(FloatWritable v : values)
 				scoreSum += v.get();
 			
-			//if there are less than topk elements, just add the current one
+			//if there are less than topk elements, add the new (docid,score) to the queue
 			if(queue.size()<topk)
 				queue.add(new PairOfStringFloat(key.getRightElement(),scoreSum));
-			//if we have topk elements in the queue, we need to check if the queue's current min. is smaller than
-			//the incoming score; if yes, remove the current min. from the heap and add the score;
-			//otherwise do nothing
+			//if we have topk elements in the queue, we need to check if the queue's current minimum is smaller than
+			//the incoming score; if yes, "exchnage" the (docid,score) elements
 			else if(queue.peek().getRightElement()<scoreSum)
 			{
 				queue.remove();
 				queue.add(new PairOfStringFloat(key.getRightElement(),scoreSum));
 			}
-			else
-				;
 		}
 		
 		
-		//don't forget the last query's content!
+		//in the cleanup, we still have to deal with the final query
 		public void cleanup(Context context) throws IOException, InterruptedException {
 
 			if(currentQuery>0 && queue.size()>0)
@@ -467,14 +457,12 @@ public class LMRetrieval extends Configured implements Tool {
 
 		job.setMapperClass(MyMapper.class);
 		job.setPartitionerClass(MyPartitioner.class);
-		job.setCombinerClass(MyCombiner.class);
 		job.setReducerClass(MyReducer.class);
 
 		long startTime = System.currentTimeMillis();
 		job.waitForCompletion(true);
 		LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime)
 				/ 1000.0 + " seconds");
-
 		return 0;
 	}
 
