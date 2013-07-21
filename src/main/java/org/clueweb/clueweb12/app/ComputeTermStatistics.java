e2a31a23c35f62fabacb53693d8b1c4742fc603a
lintool <jimmylin@umd.edu>
2013-07-21 14:18:47 +0200

    Implemented PorterAnalyzer with stopwords list from Terrier.

--- src/main/java/org/clueweb/clueweb12/app/ComputeTermStatistics.java
+++ src/main/java/org/clueweb/clueweb12/app/ComputeTermStatistics.java
@@ -42,10 +42,9 @@ import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
-import org.apache.lucene.util.Version;
 import org.clueweb.clueweb12.ClueWeb12WarcRecord;
 import org.clueweb.clueweb12.mapreduce.ClueWeb12InputFormat;
+import org.clueweb.dictionary.PorterAnalyzer;
 import org.jsoup.Jsoup;
 
 import tl.lin.data.pair.PairOfIntLong;
@@ -58,7 +57,8 @@ public class ComputeTermStatistics extends Configured implements Tool {
 
   private static enum Records { TOTAL, PAGES, ERRORS, SKIPPED };
 
-  private static final Analyzer ANALYZER = new StandardAnalyzer(Version.LUCENE_43);
+  //private static final Analyzer ANALYZER = new StandardAnalyzer(Version.LUCENE_43);
+  private static final Analyzer ANALYZER = new PorterAnalyzer();
 
   private static final String HADOOP_DF_MIN_OPTION = "df.min";
   private static final String HADOOP_DF_MAX_OPTION = "df.max";
