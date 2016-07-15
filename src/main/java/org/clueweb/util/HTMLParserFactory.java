c2a934f2d46d96a970e51aa10117ffcabae3db29
Claudia Hauff <claudia.hauff@gmail.com>
2016-07-15 12:54:05 +0200

    Separate parsing of <title> and <body> tags added.

--- src/main/java/org/clueweb/util/HTMLParserFactory.java
+++ src/main/java/org/clueweb/util/HTMLParserFactory.java
@@ -3,11 +3,13 @@ package org.clueweb.util;
 import java.io.ByteArrayInputStream;
 
 import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
 import org.apache.tika.parser.ParseContext;
 import org.apache.tika.parser.html.BoilerpipeContentHandler;
 import org.apache.tika.parser.html.HtmlParser;
 import org.apache.tika.sax.BodyContentHandler;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.xml.sax.ContentHandler;
 
 import de.l3s.boilerpipe.BoilerpipeProcessingException;
@@ -15,19 +17,49 @@ import de.l3s.boilerpipe.extractors.ArticleExtractor;
 
 public class HTMLParserFactory {
 
+  /*
+   * this method returns the content of the <body> tag 
+   */
   public static String parse(String parserType, String content) throws Exception {
+    
     if (parserType.equals("jsoup")) {
-      return Jsoup.parse(content).text();
+      return Jsoup.parse(content).body().text();
     }
-    if (parserType.equals("tika")) {
+    else if (parserType.equals("tika")) {
       ContentHandler handler = new BodyContentHandler();
       Metadata metadata = new Metadata();
       new HtmlParser().parse(new ByteArrayInputStream(content.getBytes("UTF-8")), handler,
           metadata, new ParseContext());
       return handler.toString();
     }
-
-    return null;
+    else {
+      return "";
+    }
+  }
+  
+  /*
+   * this method returns the title and the content of the <body> tag in an array
+   */
+  public static String[] parseTitleAndBody(String parserType, String content) throws Exception {
+    String[] result = new String[2];
+    
+    if (parserType.equals("jsoup")) {
+      Document doc = Jsoup.parse(content);
+      result[0] = doc.title();
+      result[1] = doc.body().text();
+    }
+    else if (parserType.equals("tika")) {
+      ContentHandler handler = new BodyContentHandler();
+      Metadata metadata = new Metadata();
+      new HtmlParser().parse(new ByteArrayInputStream(content.getBytes("UTF-8")), handler,
+          metadata, new ParseContext());
+      result[0] = metadata.get(TikaCoreProperties.TITLE); 
+      result[1] = handler.toString();
+    }
+    else
+      ;
+    
+    return result;
   }
 
   public static String getOptions() {
